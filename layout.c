5383479edfdef6dccbdb3a1ec9b29d4ba6f4267a
Aleksandr Sajenko <aleksandr.sajenko@forwardview.eu>
2021-08-15 01:39:46 +0300

    Add arrow layer
    Having a whole layer just for arrow keys is wasteful, but since this layout is a WIP, putting it there for testing purposes.

--- layout.c
+++ layout.c
@@ -23,6 +23,7 @@ enum planck_layers {
     _RAISE,
     _ADJUST,
     _NUM,
+    _ARROWS,
 };
 
 enum planck_keycodes {
@@ -30,9 +31,10 @@ enum planck_keycodes {
     COLEMAK,
 };
 
-#define LOWER MO(_LOWER)
-#define RAISE MO(_RAISE)
-#define NUM   MO(_NUM)
+#define LOWER  MO(_LOWER)
+#define RAISE  MO(_RAISE)
+#define NUM    MO(_NUM)
+#define ARROWS MO(_ARROWS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -44,14 +46,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Ctrl | Alt  | GUI  |Lower |  Num | Space| Raise| Left | Down |  Up  |Right |
+ * | Ctrl | Ctrl | Alt  | GUI  |Lower |  Num | Space| Raise| Arrow|      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = LAYOUT_planck_grid(
     KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,  KC_T,            KC_Y,   KC_U,  KC_I,    KC_O,    KC_P,    KC_BSPC,
     LGUI_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,  KC_G,            KC_H,   KC_J,  KC_K,    KC_L,    KC_SCLN, LGUI_T(KC_QUOT),
     KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,            KC_N,   KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_LCTL,        KC_LCTL, KC_LALT, KC_LGUI, LOWER, LT(NUM, KC_SPC), KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL,        KC_LCTL, KC_LALT, KC_LGUI, LOWER, LT(NUM, KC_SPC), KC_SPC, RAISE, ARROWS,  _______, _______, _______
 ),
 
 /* Colemak
@@ -62,14 +64,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Shift|   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Ctrl | Alt  | GUI  |Lower |  Num | Space| Raise| Left | Down |  Up  |Right |
+ * | Ctrl | Ctrl | Alt  | GUI  |Lower |  Num | Space| Raise| Arrow|      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_planck_grid(
     KC_TAB,         KC_Q,    KC_W,    KC_F,    KC_P,  KC_G,            KC_J,    KC_L, KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
     LGUI_T(KC_ESC), KC_A,    KC_R,    KC_S,    KC_T,  KC_D,            KC_H,    KC_N, KC_E,    KC_I,    KC_O,    LGUI_T(KC_QUOT),
     KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,            KC_K,    KC_M, KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
-    KC_LCTL,        KC_LCTL, KC_LALT, KC_LGUI, LOWER, LT(NUM, KC_SPC), KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_LCTL,        KC_LCTL, KC_LALT, KC_LGUI, LOWER, LT(NUM, KC_SPC), KC_SPC, RAISE, ARROWS,  _______, _______, _______
 ),
 
 /* Lower
@@ -142,6 +144,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_0,    KC_4,    KC_5,    KC_6,    KC_MINS, KC_MINS, KC_4,    KC_5,     KC_6,    _______, _______,
     _______, KC_0,    KC_1,    KC_2,    KC_3,    KC_DOT,  KC_DOT,  KC_1,    KC_2,     KC_3,    _______, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_0,    _______,  _______, _______, _______
+),
+
+/* Arrows
+ * ,-----------------------------------------------------------------------------------.
+ * |      |      |      |      |      |      |      |      |  Up  |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      | Left | Down |Right |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
+ * `-----------------------------------------------------------------------------------'
+ */
+[_ARROWS] = LAYOUT_planck_grid(
+    _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,    _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN,  KC_RGHT, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
 )
 };
 
