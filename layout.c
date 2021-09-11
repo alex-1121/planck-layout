40e8f25c32c26b20e9645d94ca3899ba1df70b36
Aleksandr Sajenko <aleksandr.sajenko@forwardview.eu>
2021-09-11 15:41:52 +0300

    Add mute button to Media layer

--- layout.c
+++ layout.c
@@ -1,19 +1,3 @@
-/* Copyright 2015-2017 Jack Humbert
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #include QMK_KEYBOARD_H
 
 enum planck_layers {
@@ -148,7 +132,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Media
  * ,-----------------------------------------------------------------------------------.
- * |      |      |      |      |      |      |      |      | Vol- | Play |      |      |
+ * |      |      |      |      |      |      |      | Mute | Vol- | Play |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      | Prev | Vol+ | Next |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -158,7 +142,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_MEDIA] = LAYOUT_planck_grid(
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU,  KC_MPLY, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, KC__MUTE,KC_VOLU,  KC_MPLY, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, KC_MPRV, KC_VOLD,  KC_MNXT, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, KC_MPRV, KC_MNXT
